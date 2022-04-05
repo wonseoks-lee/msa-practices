a806d02bc340637a466c028bdaf156094a9b23e5
Wonseok <ls4050@naver.com>
2022-04-05 12:38:23 +0900

    first commit

---
+++ servers/service-registry-server/src/main/java/me/kickscar/msa/server/ServiceRegistryServer.java
@@ -0,0 +1,15 @@
+package me.kickscar.msa.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class ServiceRegistryServer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceRegistryServer.class, args);
+	}
+
+}
